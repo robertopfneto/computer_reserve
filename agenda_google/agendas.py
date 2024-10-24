eb3425eeb2a8940861ebe188ae4d676c78c879e6
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-10-24 17:02:14 -0400

    Update agendas.py

--- agenda_google/agendas.py
+++ agenda_google/agendas.py
@@ -7,26 +7,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
-# Escopos de permissão para a Google Calendar API
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-def authenticate_google_calendar():
-    """Autentica o Google Calendar e retorna o serviço de acesso"""
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    
-    service = build('calendar', 'v3', credentials=creds)
-    return service
+from agenda_google.authenticate import authenticate_google_calendar
 
 def list_gpus_available(service, calendar_id, date=None):
     """Lista as GPUs disponíveis em um determinado dia"""
